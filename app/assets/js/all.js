bb403e861f733ea8ca6136f711c07800f3dcb6fe
berylpu <berylpu627@gmail.com>
2022-09-13 22:11:29 +0800

    update homepage

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,42 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// courseSwiper
+var swiper = new Swiper(".courseSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+  speed: 1000,
+  centeredSlides: true,
+  autoplay: {
+    delay: 100,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 10,
+      spaceBetween: 16,
+    },
+  }
+});
+//swiper
+var swiper2 = new Swiper(".feedback-swiper", {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+// Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
